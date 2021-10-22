1926f3995ac27f93044fc2f0a1888923f2d8ceeb
Jacob <MightyMelonJL@gmail.com>
2021-10-22 09:00:07 -0400

    added jacobs lab assets, code, and scene

---
+++ Build-A-Brawl/Assets/Scripts/LabScripts/explosion.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class explosion : MonoBehaviour
+{
+    public GameObject explode, bits;
+    public float explosionStrength, limit;
+    // Start is called before the first frame update
+    
+
+    private void OnCollisionEnter(Collision collision){
+        GameObject _explosion = Instantiate(explode,transform.position, transform.rotation);
+        GameObject _bits = Instantiate(bits,transform.position, transform.rotation);
+        Destroy(_explosion, 3);
+        Destroy(_bits, 5);
+        knockBack();
+        Destroy(gameObject);
+    }
+
+    void knockBack() {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, limit);
+
+        foreach (Collider close in colliders){
+            Rigidbody rigg = close.GetComponent<Rigidbody>();
+            if (rigg != null){
+                rigg.AddExplosionForce(explosionStrength, transform.position, limit);
+            }
+        }
+    }
+}
