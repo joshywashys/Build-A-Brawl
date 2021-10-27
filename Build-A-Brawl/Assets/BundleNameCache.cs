ae837986fc74ff7222be5791213501a9337b5800
Tristan Anderson <tristananderson6500@gmail.com>
2021-10-27 14:57:36 -0400

    Adding a preferences editor window to allow for setting custom BundleNameCache variable names.

--- Build-A-Brawl/Assets/BundleNameCache.cs
+++ Build-A-Brawl/Assets/BundleNameCache.cs
@@ -1,8 +1,6 @@
-// This file has been auto-generated after last AssetBundle build on 10/27/2021 12:11:03 AM
 public static class BundleNameCache {
-	public static string creatureparts = "CreatureParts";
-	public static string creaturepartsArms = "CreatureParts\\Arms";
-	public static string creaturepartsHeads = "CreatureParts\\Heads";
-	public static string creaturepartsLegs = "CreatureParts\\Legs";
-	public static string creaturepartsTorsos = "CreatureParts\\Torsos";
+	public static string creaturepartsArms = "creatureparts/arms";
+	public static string creaturepartsHeads = "creatureparts/heads";
+	public static string creaturepartsLegs = "creatureparts/legs";
+	public static string creaturepartsTorsos = "creatureparts/torsos";
 }
--- Build-A-Brawl/Assets/Editor/CreateAssetBundles.cs
+++ Build-A-Brawl/Assets/Editor/CreateAssetBundles.cs
@@ -3,8 +3,10 @@ using UnityEngine;
 using System.IO;
 using System.Collections.Generic;
 
-public class CreateAssetBundles
+public static class CreateAssetBundles
 {
+	static Dictionary<string, string> assetBundleNicknames;
+
 	[MenuItem("Assets/Build All AssetBundles")]
 	static void BuildAllAssetBundles()
 	{
@@ -15,54 +17,272 @@ public class CreateAssetBundles
 			Directory.CreateDirectory(assetBundleDirectory);
 
 		List<string> outBundleNames = new List<string>();
+		List<AssetBundlePathLink> outLinks = new List<AssetBundlePathLink>();
 		foreach (string directory in Directory.GetDirectories(Path.Combine(Application.dataPath, "BundledAssets")))
-			RecursiveDirectoryAssetScan(directory, ref outBundleNames);
+			RecursiveDirectoryAssetScan(directory, ref outLinks, ref outBundleNames);
 
+		SetAssetBundleNames(outLinks);
+
+		AssetDatabase.RemoveUnusedAssetBundleNames();
 		BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.None, EditorUserBuildSettings.activeBuildTarget);
 
-		CreateBundleNameCache(outBundleNames);
+		CreateBundleNameCache();
 
 		AssetDatabase.Refresh();
 	}
 
-	static void CreateBundleNameCache(List<string> bundleNames)
+	static void LoadNicknames()
     {
+		assetBundleNicknames = new Dictionary<string, string>();
+		foreach (string name in AssetDatabase.GetAllAssetBundleNames())
+			assetBundleNicknames.Add(name, name);
+
+		string editorPath = Path.Combine(Application.dataPath, "Editor/BuildAssetBundlePrefs.json");
+		if (!File.Exists(editorPath))
+		{
+			Debug.LogWarning("Failed to load BuildAssetBundlePrefs.json - file not found. Loading defaults...");
+			return;
+		}
+
+		int assetBundleCount = assetBundleNicknames.Count;
+		string[] data = File.ReadAllText(editorPath).Split('\n');
+		for (int i = 1; i <= assetBundleCount; i++)
+		{
+			if (i < assetBundleCount)
+				data[i] = data[i].Remove(data[i].Length - 1);
+
+			BuildAssetBundlesPreferencesWindow.AssetBundleNickname nickname = JsonUtility.FromJson<BuildAssetBundlesPreferencesWindow.AssetBundleNickname>(data[i]);
+			if (!assetBundleNicknames.ContainsKey(nickname.assetBundleName))
+				continue;
+
+			assetBundleNicknames[nickname.assetBundleName] = nickname.assetBundleNickname;
+		}
+	}
+
+	public static void CreateBundleNameCache()
+    {
+		LoadNicknames();
+
 		using (StreamWriter outfile =
 			new StreamWriter(Path.Combine(Application.dataPath, "BundleNameCache.cs")))
 		{
-			outfile.WriteLine($"// This file has been auto-generated after last AssetBundle build on {System.DateTime.Now}");
 			outfile.WriteLine("public static class BundleNameCache {");
-			for (int i = 0; i < bundleNames.Count; i++)
+			foreach (KeyValuePair<string, string> nickname in assetBundleNicknames)
 			{
-				string[] splitName = bundleNames[i].Split('\\');
+				string[] splitName = nickname.Value.Split(new char[] { '\\', '/' });
 				string varName = "";
-				for (int j = 0; j < splitName.Length; j++)
+				for (int i = 0; i < splitName.Length; i++)
 				{
-					if (j == 0)
-						splitName[j] = splitName[j].ToLower();
-					varName += splitName[j];
+					if (i > 0)
+						splitName[i] = char.ToUpper(splitName[i][0]) + splitName[i].Substring(1);
+					varName += splitName[i];
 				}
-				string bundleName = bundleNames[i].Replace("\\", "\\\\");
+				string bundleName = nickname.Key.Replace("\\", "\\\\");
 				outfile.WriteLine($"\tpublic static string {varName} = \"{bundleName}\";");
 			}
 			outfile.WriteLine("}");
 		}
 	}
 
-	static void RecursiveDirectoryAssetScan(string currentDirectory, ref List<string> outBundleNames)
+	struct AssetBundlePathLink
+	{
+		public AssetBundlePathLink(string path, string bundle)
+		{
+			this.path = path;
+			this.bundle = bundle;
+		}
+
+		public string path;
+		public string bundle;
+	}
+
+	static void SetAssetBundleNames(List<AssetBundlePathLink> links)
+    {
+		foreach (AssetBundlePathLink asset in links)
+			AssetImporter.GetAtPath(asset.path).SetAssetBundleNameAndVariant(asset.bundle, "");
+	}
+
+	static void RecursiveDirectoryAssetScan(string currentDirectory, ref List<AssetBundlePathLink> outAssetPaths, ref List<string> outBundleNames)
 	{
 		string searchPattern = "*.prefab";
 
 		string label = currentDirectory.Replace(Path.Combine(Application.dataPath, "BundledAssets\\"), "");
 		foreach(string assetPath in Directory.GetFiles(currentDirectory, searchPattern))
-		{
-			string projectRelativeDirectory = assetPath.Replace(Application.dataPath, "Assets");
-			AssetImporter.GetAtPath(projectRelativeDirectory).SetAssetBundleNameAndVariant(label, "");
-		}
+			outAssetPaths.Add(new AssetBundlePathLink(assetPath.Replace(Application.dataPath, "Assets"), label));
+		
 		outBundleNames.Add(label);
 
 		string[] directories = Directory.GetDirectories(currentDirectory);
 		foreach(string directory in directories)
-			RecursiveDirectoryAssetScan(directory, ref outBundleNames);
+			RecursiveDirectoryAssetScan(directory, ref outAssetPaths, ref outBundleNames);
+	}
+}
+
+public class BuildAssetBundlesPreferencesWindow : EditorWindow
+{
+	Dictionary<string, string> assetBundleNicknames;
+
+	GUIStyle headerStyle, subHeaderStyle;
+
+	[MenuItem("Assets/Build AssetBundles Preferences")]
+	static void Init()
+	{
+		BuildAssetBundlesPreferencesWindow prefWindow = (BuildAssetBundlesPreferencesWindow)GetWindow(typeof(BuildAssetBundlesPreferencesWindow));
+		prefWindow.titleContent = new GUIContent("Build AssetBundle Preferences");
+
+		GUIStyle headerStyle = new GUIStyle();
+		headerStyle.fontSize = 20;
+		headerStyle.fontStyle = FontStyle.Bold;
+		headerStyle.normal.textColor = new Color(0.85f, 0.85f, 0.85f);
+		headerStyle.padding = new RectOffset(10, 10, 5, 5);
+		prefWindow.headerStyle = headerStyle;
+
+		GUIStyle subHeaderStyle = new GUIStyle(headerStyle);
+		subHeaderStyle.fontSize = 16;
+		subHeaderStyle.padding = new RectOffset(5, 5, 2, 2);
+		prefWindow.subHeaderStyle = subHeaderStyle;
+
+		prefWindow.Refresh();
+		prefWindow.Show();
+	}
+
+	void Refresh()
+	{
+		assetBundleNicknames = new Dictionary<string, string>();
+		foreach (string name in AssetDatabase.GetAllAssetBundleNames())
+		{
+			string[] splitName = name.Split(new char[] { '\\', '/' });
+			string nickname = "";
+			for (int i = 0; i < splitName.Length; i++)
+			{
+				if (i > 0)
+					splitName[i] = char.ToUpper(splitName[i][0]) + splitName[i].Substring(1);
+				nickname += splitName[i];
+			}
+			assetBundleNicknames.Add(name, nickname);
+		}
+
+		LoadSettings();
+	}
+
+	[System.Serializable]
+	public struct AssetBundleNickname
+    {
+		public AssetBundleNickname(string name, string nickname)
+        {
+			assetBundleName = name;
+			assetBundleNickname = nickname;
+        }
+
+		public string assetBundleName;
+		public string assetBundleNickname;
+    }
+
+	void ApplySettings()
+    {
+		string data = "{\"AssetBundle Nicknames\" : [\n";
+		
+		int currentIndex = 0;
+		foreach (KeyValuePair<string, string> nickname in assetBundleNicknames)
+			data += JsonUtility.ToJson(new AssetBundleNickname(nickname.Key, nickname.Value)) + ((currentIndex++ == assetBundleNicknames.Count - 1) ? "" : ",") + '\n';
+		
+		data += "]}";
+
+		string editorPath = Path.Combine(Application.dataPath, "Editor/BuildAssetBundlePrefs.json");
+		File.WriteAllText(editorPath, data);
+
+		CreateAssetBundles.CreateBundleNameCache();
+		
+		AssetDatabase.Refresh();
+	}
+
+	void LoadSettings()
+    {
+		string editorPath = Path.Combine(Application.dataPath, "Editor/BuildAssetBundlePrefs.json");
+		if (!File.Exists(editorPath))
+		{
+			Debug.LogWarning("Failed to load BuildAssetBundlePrefs.json - file not found. Loading defaults...");
+			return;
+		}
+
+		int assetBundleCount = assetBundleNicknames.Count;
+		string[] data = File.ReadAllText(editorPath).Split('\n');
+		for (int i = 1; i <= assetBundleCount; i++)
+		{
+			if (i < assetBundleCount)
+				data[i] = data[i].Remove(data[i].Length - 1);
+
+			AssetBundleNickname nickname = JsonUtility.FromJson<AssetBundleNickname>(data[i]);
+			if (!assetBundleNicknames.ContainsKey(nickname.assetBundleName))
+				continue;
+
+			assetBundleNicknames[nickname.assetBundleName] = nickname.assetBundleNickname;
+		}
+	}
+
+	void Default()
+    {
+		AssetBundleNickname[] tempNicknames = new AssetBundleNickname[assetBundleNicknames.Count];
+		int currentIndex = 0;
+		foreach (string name in assetBundleNicknames.Keys)
+		{
+			string[] splitName = name.Split(new char[] { '\\', '/' });
+			string nickname = "";
+			for (int i = 0; i < splitName.Length; i++)
+			{
+				if (i > 0)
+					splitName[i] = char.ToUpper(splitName[i][0]) + splitName[i].Substring(1);
+				nickname += splitName[i];
+			}
+			tempNicknames[currentIndex] = new AssetBundleNickname(name, nickname);
+			currentIndex++;
+		}
+
+		for (int i = 0; i < tempNicknames.Length; i++)
+			assetBundleNicknames[tempNicknames[i].assetBundleName] = tempNicknames[i].assetBundleNickname;
+	}
+
+    private void OnValidate()
+    {
+		Refresh();
+    }
+
+    void OnGUI()
+	{
+		// Header
+		GUILayout.Label("Build AssetBundle Preferences", headerStyle);
+		GUILayout.Space(25);
+
+		// Nicknames
+		GUILayout.Label("AssetBundle Nicknames", subHeaderStyle);
+		{
+			AssetBundleNickname[] tempNicknames = new AssetBundleNickname[assetBundleNicknames.Count];
+			int currentIndex = 0;
+			foreach (KeyValuePair<string, string> nickname in assetBundleNicknames)
+			{
+				tempNicknames[currentIndex] = new AssetBundleNickname(nickname.Key, nickname.Value);
+
+				GUILayout.BeginHorizontal();
+				{
+					GUILayout.Label(nickname.Key);
+					tempNicknames[currentIndex].assetBundleNickname = GUILayout.TextField(nickname.Value);
+				}
+				GUILayout.EndHorizontal();
+
+				currentIndex++;
+			}
+
+			for (int i = 0; i < tempNicknames.Length; i++)
+				assetBundleNicknames[tempNicknames[i].assetBundleName] = tempNicknames[i].assetBundleNickname;
+		}
+
+		// Footer Buttons
+		GUILayout.BeginHorizontal();
+		{
+			if (GUILayout.Button("Refresh")) Refresh();
+			if (GUILayout.Button("Default")) Default();
+			if (GUILayout.Button("Apply")) ApplySettings();
+		}
+		GUILayout.EndHorizontal();
 	}
 }
