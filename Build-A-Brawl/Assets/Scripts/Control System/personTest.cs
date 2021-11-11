9c698662f776964e26d2fa6daf3e88f68497daa3
annascheidl <anna.scheidl@outlook.com>
2021-11-11 01:20:39 -0500

    Movement + Rotation
    I've got movement and rotation down, editing more later for jump + going to add comments to explain everything on Thursday. Hierarchy box for the traffic map was cleaned up

--- Build-A-Brawl/Assets/Scripts/Control System/personTest.cs
+++ Build-A-Brawl/Assets/Scripts/Control System/personTest.cs
@@ -5,41 +5,54 @@ using UnityEngine.InputSystem;
 
 [Serializable]
 
-public class MoveInputEvent : UnityEvent<float, float> { }
 
+//classes declared
+public class MoveInputEvent : UnityEvent<float, float> { }
+public class JumpInputEvent : UnityEvent<float, float> { }
 public class personTest : MonoBehaviour
 {
     Controls playerControls;
+    
+    //declare instance of the classes
     public MoveInputEvent moveInputEvent;
+    public JumpInputEvent jumpInputEvent;
+
     Vector3 move;
 
 
     void Awake()
     {
+        //Set up specific class controls
         playerControls = new Controls();
-
-        playerControls.Gameplay.Jump.performed += ctx => Jump();
-
     }
 
-    void Jump()
-    {
-        transform.position += Vector3.up * 1.0f;
-   
-    }
+    
 
+    // When created, register performed actions here
     void OnEnable()
     {
+        
         playerControls.Gameplay.Enable();
         playerControls.Gameplay.Move.performed += OnMovePerformed;
         playerControls.Gameplay.Move.canceled += OnMovePerformed;
+        playerControls.Gameplay.Jump.performed += OnJumpPerformed;
+        playerControls.Gameplay.Jump.canceled += OnJumpPerformed;
     }
 
+    // movement action function
     void OnMovePerformed(InputAction.CallbackContext context)
     {
         Vector2 moveInput = context.ReadValue<Vector2>();
         moveInputEvent.Invoke(moveInput.x, moveInput.y);
        // Debug.Log($"Player Controller: Move Input:{moveInput}");
+    }
+
+    void OnJumpPerformed(InputAction.CallbackContext context)
+    {
+        Vector2 jumpInput = context.ReadValue<Vector2>();
+        jumpInputEvent.Invoke(jumpInput.x, jumpInput.y);
+        transform.position += Vector3.up * 1.0f;
+
     }
 
     void OnDisable()
--- Build-A-Brawl/Assets/Scripts/Control System/playerController.cs
+++ Build-A-Brawl/Assets/Scripts/Control System/playerController.cs
@@ -2,10 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//will go through and put comments on Nov 12th
+
 public class playerController : MonoBehaviour
 {
-    public float moveVelocity = 5.0f;
+    public float speed = 5.0f;
     public float rotationSpeed = 280.0f;
+    public float jumpVelocity = 5.0f;
 
     float horizontal;
     float vertical;
@@ -13,20 +16,25 @@ public class playerController : MonoBehaviour
     private void Update()
     {
         Vector3 moveDirection = Vector3.forward * vertical + Vector3.right * horizontal;
+        
 
+        Vector3 proCamForward = Vector3.ProjectOnPlane(Camera.main.transform.forward, Vector3.up);
+        Quaternion rotationToCamera = Quaternion.LookRotation(proCamForward, Vector3.up);
+        
+
+        moveDirection = rotationToCamera * moveDirection;
         Quaternion rotationToMoveDirection = Quaternion.LookRotation(moveDirection, Vector3.up);
 
-        transform.rotation = Quaternion.RotateTowards(transform.rotation, rotationToMoveDirection, rotationSpeed * Time.deltaTime);
-        
+
 
         if (moveDirection.magnitude > 0)
         {
             rotationToMoveDirection = Quaternion.LookRotation(moveDirection);
             transform.rotation = Quaternion.RotateTowards(transform.rotation, rotationToMoveDirection, rotationSpeed * Time.deltaTime);
         }
-
-        transform.position += moveDirection * moveVelocity * Time.deltaTime;
-
+        
+        transform.position += moveDirection * speed * Time.deltaTime;
+        
     }
 
     public void OnMoveInput(float horizontal, float vertical)
@@ -34,5 +42,10 @@ public class playerController : MonoBehaviour
         this.vertical = vertical;
         this.horizontal = horizontal;
         Debug.Log($"Player Controller: Move Input: {vertical}, {horizontal}");
+    }
+
+    public void OnJumpInput (float vertical)
+    {
+        this.vertical = vertical;
     }
 }
