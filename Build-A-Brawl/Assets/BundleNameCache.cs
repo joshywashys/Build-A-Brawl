c505fdd0102d65d08882a85dcf0d2dc68b43c178
joshywashys <joshuastrutt1@gmail.com>
2022-03-11 14:15:31 -0500

    robot arm rigged

--- Build-A-Brawl/Assets/BundleNameCache.cs
+++ Build-A-Brawl/Assets/BundleNameCache.cs
@@ -1,5 +1,4 @@
 public static class BundleNameCache {
-	public static string creatureparts = "creatureparts";
 	public static string creaturepartsArmsL = "creatureparts/armsl";
 	public static string creaturepartsArmsR = "creatureparts/armsr";
 	public static string creaturepartsHeads = "creatureparts/heads";
