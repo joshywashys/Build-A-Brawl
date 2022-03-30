9e8ff824045874ad9d664d566d03b62a9390ef04
joshywashys <joshuastrutt1@gmail.com>
2022-03-30 15:49:12 -0400

    CC Tooltip UpdatePosition

--- Build-A-Brawl/Assets/Scripts/Creature Creator/CCToolTip.cs
+++ Build-A-Brawl/Assets/Scripts/Creature Creator/CCToolTip.cs
@@ -8,6 +8,10 @@ public class CCToolTip : MonoBehaviour
     public PartCombiner creature;
     public GameObject symbolPrefab;
     private GameObject tooltip;
+    private Camera cam;
+
+    private bool selected;
+    private GameObject selectedPart;
 
     // Called on PartCombiner partswap
     public void UpdateSymbols()
@@ -29,12 +33,32 @@ public class CCToolTip : MonoBehaviour
         }
     }
 
+    public void UpdatePosition(GameObject target)
+    {
+        Vector3 newPos = cam.WorldToScreenPoint(target.transform.position);
+        tooltip.transform.position = newPos;
+    }
+
     public void Start()
     {
         tooltip = gameObject;
+        cam = FindObjectOfType<Camera>();
 
         creature.onPartSwap.AddListener(UpdateSymbols);
+
+        Show(); // DEBUGGING
     }
+
+    public void Update()
+    {
+        if (true)
+        {
+            selectedPart = creature.currHead; // DEBUGGING // replace with actual selected part
+            UpdatePosition(selectedPart);
+        }
+    }
+
+    //maybe i can add in a function to make them appear not all at once
 
     public void Show()
     {
