992a69e9f90c7b5e5dca5dccbd0431f040f63d97
Tristan Anderson <tristananderson6500@gmail.com>
2022-03-30 15:47:36 -0400

    Bug fixes - involving grabbing and throwing.

--- Build-A-Brawl/Assets/Scripts/throwableObject.cs
+++ Build-A-Brawl/Assets/Scripts/throwableObject.cs
@@ -1,19 +1,49 @@
+using System;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
 public class ThrowableObject : MonoBehaviour
 {
-    [HideInInspector] 
-    public new Rigidbody rigidbody;
-    public new Collider collider;
+	[HideInInspector]
+	public Transform holder;
+	[HideInInspector]
+	public new Rigidbody rigidbody;
 
-    public void Start()
-    {
-        rigidbody = GetComponent<Rigidbody>();
-    }
+	public new Collider collider;
+	public Transform[] holdPoints;
 
-    public void IsGrabbed(bool pickedUp)
-    {
-        collider.enabled = !pickedUp;
-    }
+	public bool IsGrabbed { get; private set; } = false;
+
+	public void Start()
+	{
+		rigidbody = GetComponent<Rigidbody>();
+	}
+
+	public void SetGrabbed(bool pickedUp, Transform grabber = null)
+	{
+		collider.enabled = !pickedUp;
+		IsGrabbed = pickedUp;
+
+		if (pickedUp)
+			holder = grabber;
+	}
+
+	internal bool IsHolder(Transform transform)
+	{
+		if (holder == null)
+			return false;
+		return holder.root == transform.root;
+	}
+
+#if UNITY_EDITOR
+	[Header("Debug")]
+	[SerializeField] private float gizmoSize = 0.25f;
+	[SerializeField] private Color gizmoColour = Color.blue;
+	private void OnDrawGizmos()
+	{
+		Gizmos.color = gizmoColour;
+		foreach (Transform point in holdPoints)
+			Gizmos.DrawWireSphere(point.position, gizmoSize);
+	}
+#endif
 }
