28438669e68c545450b429f74d661e590ccd14fe
Jacob <MightyMelonJL@gmail.com>
2022-04-03 12:29:48 -0400

    characters dont jitter

---
+++ Build-A-Brawl/Assets/Scripts/BugFixScripts/deleteUselessColliders.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class deleteUselessColliders : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public BoxCollider boxColliders;
+    public SphereCollider sphereColliders;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        boxColliders = this.GetComponent<BoxCollider>();
+        sphereColliders = this.GetComponent<SphereCollider>();
+        Destroy(boxColliders);
+        Destroy(sphereColliders);
+    }
+}
