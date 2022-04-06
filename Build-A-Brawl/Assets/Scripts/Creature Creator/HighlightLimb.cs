51f39b65ac836e3d83643016bdab65bde34c56ca
joshywashys <joshuastrutt1@gmail.com>
2022-04-06 05:05:42 -0400

    highlight changes

--- Build-A-Brawl/Assets/Scripts/Creature Creator/HighlightLimb.cs
+++ Build-A-Brawl/Assets/Scripts/Creature Creator/HighlightLimb.cs
@@ -20,50 +20,53 @@ public class HighlightLimb : MonoBehaviour
     //drag this onto all partcombiner OnPartSwap Events
     public void AddHighlight()
     {
+        
         if (limb == Limbs.head)
         {
-            if (partCombiner.newHead.GetComponent<PlayerOutline>() == null) { partCombiner.newHead.AddComponent<PlayerOutline>(); }
+            if (partCombiner.newHead.GetComponent<PlayerOutline>() != null) { partCombiner.newHead.GetComponent<PlayerOutline>().enabled = true; }
         }
         if (limb == Limbs.torso)
         {
-            if (partCombiner.newTorso.GetComponent<PlayerOutline>() == null) { partCombiner.newTorso.AddComponent<PlayerOutline>(); }
+            if (partCombiner.newTorso.GetComponent<PlayerOutline>() != null) { partCombiner.newTorso.GetComponent<PlayerOutline>().enabled = true; }
         }
         if (limb == Limbs.armL)
         {
-            if (partCombiner.newArmL.GetComponent<PlayerOutline>() == null) { partCombiner.newArmL.AddComponent<PlayerOutline>(); }
+            if (partCombiner.newArmL.GetComponent<PlayerOutline>() != null) { partCombiner.newArmL.GetComponent<PlayerOutline>().enabled = true; }
         }
         if (limb == Limbs.armR)
         {
-            if (partCombiner.newArmR.GetComponent<PlayerOutline>() == null) { partCombiner.newArmR.AddComponent<PlayerOutline>(); }
+            if (partCombiner.newArmR.GetComponent<PlayerOutline>() != null) { partCombiner.newArmR.GetComponent<PlayerOutline>().enabled = true; }
         }
         if (limb == Limbs.legs)
         {
-            if (partCombiner.newLegs.GetComponent<PlayerOutline>() == null) { partCombiner.newLegs.AddComponent<PlayerOutline>(); }
+            if (partCombiner.newLegs.GetComponent<PlayerOutline>() != null) { partCombiner.newLegs.GetComponent<PlayerOutline>().enabled = true; }
         }
+        
     }
 
     public void RemoveHighlight()
     {
         if (limb == Limbs.head)
         {
-            Destroy(partCombiner.newHead.GetComponent<PlayerOutline>());
+            if (partCombiner.newHead.GetComponent<PlayerOutline>() != null) { partCombiner.newHead.GetComponent<PlayerOutline>().enabled = false; }
         }
         if (limb == Limbs.torso)
         {
-            Destroy(partCombiner.newTorso.GetComponent<PlayerOutline>());
+            if (partCombiner.newTorso.GetComponent<PlayerOutline>() != null) { partCombiner.newTorso.GetComponent<PlayerOutline>().enabled = false; }
         }
         if (limb == Limbs.armL)
         {
-            Destroy(partCombiner.newArmL.GetComponent<PlayerOutline>());
+            if (partCombiner.newArmL.GetComponent<PlayerOutline>() != null) { partCombiner.newArmL.GetComponent<PlayerOutline>().enabled = false; }
         }
         if (limb == Limbs.armR)
         {
-            Destroy(partCombiner.newArmR.GetComponent<PlayerOutline>());
+            if (partCombiner.newArmR.GetComponent<PlayerOutline>() != null) { partCombiner.newArmR.GetComponent<PlayerOutline>().enabled = false; }
         }
         if (limb == Limbs.legs)
         {
-            Destroy(partCombiner.newLegs.GetComponent<PlayerOutline>());
+            if (partCombiner.newLegs.GetComponent<PlayerOutline>() != null) { partCombiner.newLegs.GetComponent<PlayerOutline>().enabled = false; }
         }
+
     }
 
 }
